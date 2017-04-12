f7655afce4a0450704961ad59d3210063e1197b8
miwaniza <miwaniza@gmail.com>
2017-04-12 10:26:58 +0300

    Img

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var bodyParser = require('body-parser');
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-//app.use(express.static(path.join(__dirname, 'img')));
+app.use('/img', express.static(path.join(__dirname + '/img')));
 
 
 app.get('/', function (req, res) {
